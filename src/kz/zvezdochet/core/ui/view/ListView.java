42a37e0a434c05a1f1ade2fec678a01eb3706901
nataly <info@zvezdochet.kz>
2014-08-03 20:27:32 +0600

    Раскрашиваем ячейки таблицы

--- src/kz/zvezdochet/core/ui/view/ListView.java
+++ src/kz/zvezdochet/core/ui/view/ListView.java
@@ -7,10 +7,12 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.viewers.ITableColorProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -86,14 +88,24 @@ public abstract class ListView extends View {
 	 * Стандартный класс отображения содержимого таблицы
 	 */
 	protected class ArrayLabelProvider extends LabelProvider
-									implements ITableLabelProvider {
+									implements ITableLabelProvider, ITableColorProvider {
+		@Override
 		public Image getColumnImage(Object element, int columnIndex) {
 			return null;
 		}
+		@Override
 		public String getColumnText(Object element, int columnIndex) {
 			Object[] array = (Object[])element;
 			Object val = array[columnIndex];
 			return (null == val) ? "" : val.toString();
+		}
+		@Override
+		public Color getForeground(Object element, int columnIndex) {
+			return null;
+		}
+		@Override
+		public Color getBackground(Object element, int columnIndex) {
+			return null;
 		}
 	}	
 
