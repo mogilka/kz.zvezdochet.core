96c1de3bc3350f5671fffc4f5199b90d4b28a632
nataly <info@zvezdochet.kz>
2014-09-06 22:03:52 +0600

    Доработка для использования фона ячеек и цветов шрифта

--- src/kz/zvezdochet/core/ui/view/ModelLabelProvider.java
+++ src/kz/zvezdochet/core/ui/view/ModelLabelProvider.java
@@ -1,14 +1,16 @@
 package kz.zvezdochet.core.ui.view;
 
+import kz.zvezdochet.core.ui.ArrayLabelProvider;
+
 import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 
 /**
  * Обработчик отображения модели в таблице
  * @author Nataly Didenko
  */
-public class ModelLabelProvider extends LabelProvider implements ITableLabelProvider {
+public class ModelLabelProvider extends ArrayLabelProvider implements ITableLabelProvider {
 	@Override
 	public Image getColumnImage(Object element, int columnIndex) {
 		return null;
@@ -17,4 +19,12 @@ public class ModelLabelProvider extends LabelProvider implements ITableLabelProv
 	public String getColumnText(Object element, int columnIndex) {
 		return (null == element) ? "" : element.toString(); //$NON-NLS-1$
 	}
-}	
+	@Override
+	public Color getForeground(Object element, int columnIndex) {
+		return null;
+	}
+	@Override
+	public Color getBackground(Object element, int columnIndex) {
+		return null;
+	}
+}
