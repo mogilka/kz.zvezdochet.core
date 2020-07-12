cb94e8169a705c359d574ad8aafebff54a15377a
Natalie Didenko <inbox@zvezdochet.guru>
2020-07-12 18:21:29 +0600

    Адаптация к MacOS

---
+++ src/kz/zvezdochet/core/util/OsUtil.java
@@ -0,0 +1,35 @@
+package kz.zvezdochet.core.util;
+
+/**
+ * Проверка, в какой операционной системе запущена программа
+ * @author Memin
+ * @link https://stackoverflow.com/questions/228477/how-do-i-programmatically-determine-operating-system-in-java
+ */
+public class OsUtil {
+
+	/**
+	 * Operating systems
+	 */
+	public enum OS {
+        WINDOWS, LINUX, MAC, SOLARIS
+    };
+
+    private static OS os = null;
+
+	public static OS getOS() {
+	    if (os == null) {
+	        String operSys = System.getProperty("os.name").toLowerCase();
+	        if (operSys.contains("win")) {
+	            os = OS.WINDOWS;
+	        } else if (operSys.contains("nix") || operSys.contains("nux")
+	                || operSys.contains("aix")) {
+	            os = OS.LINUX;
+	        } else if (operSys.contains("mac")) {
+	            os = OS.MAC;
+	        } else if (operSys.contains("sunos")) {
+	            os = OS.SOLARIS;
+	        }
+	    }
+	    return os;
+	}
+}
